f25d682132a6b2b3e49315e95f86584264ca35c6
GuanCun <linguancun@163.com>
2024-03-28 17:30:41 +0800

    feat: first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# rollup 打包示例
+
+> 本示例演示了如何使用 rollup 打包一个 UI 组件库，并且不发布的情况下被其他项目引用。
+
+### 技术栈
+
+monorepo + rollup + typescript + vue3
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import App1 from "app1";
+
+createApp(App).use(App1).mount("#app");
